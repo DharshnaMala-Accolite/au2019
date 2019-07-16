1f92511ae4e74abdf02cf67e24a8538c9cc5057d
dharshnamala.balakrishnana@accoliteindia.com <dharshnamala.balakrishnan@accoliteindia.com>
2019-07-16 21:48:07 +0530

    Collections Assignment on 15 july 2019,am

--- 10-July-pm/UseAnnot.java
+++
@@ -1,9 +0,0 @@
-package com.accolite.au;
-
-@Servlet(servletName = "controlServlet", urlPattern = "*.html")
-public class UseAnnot {
-void print()
-{
-	System.out.println("UseAnnot class uses the custom Servlet Annotation");
-}
-}

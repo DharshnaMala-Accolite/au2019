53e965bec02378960b9cf511b3d9ae71e5f3c681
dharshnamala.balakrishnana@accoliteindia.com <dharshnamala.balakrishnan@accoliteindia.com>
2019-07-16 21:46:30 +0530

    Collections Assignment on 15 july 2019,am

--- 10-July-pm/Servlet.java
+++
@@ -1,9 +0,0 @@
-package com.accolite.au;
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-public @interface Servlet {
-	String servletName();
-	String urlPattern();
-}
